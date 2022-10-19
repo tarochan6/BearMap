e6e6696cceadc822d4926bf96053bf578471bbe0
waon <waon@gmail.com>
2022-10-19 13:10:39 +0900

    AuthFilterの適用

--- src/main/java/com/example/app/config/ApplicationConfig.java
+++ src/main/java/com/example/app/config/ApplicationConfig.java
@@ -1,11 +1,14 @@
 package com.example.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.app.filter.AuthFilter;
 
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer{
@@ -23,6 +26,14 @@ public class ApplicationConfig implements WebMvcConfigurer{
 		var messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("validation");
 		return messageSource;
+	}
+	
+	// 認証用フィルタの有効化
+	@Bean
+	public FilterRegistrationBean<AuthFilter> authFilter() {
+	var bean = new FilterRegistrationBean<AuthFilter>(new AuthFilter());
+	bean.addUrlPatterns("/admins/*");
+	return bean;
 	}
 
 }
